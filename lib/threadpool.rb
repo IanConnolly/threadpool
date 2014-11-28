711fef92697e7b0730e8789d05e76f65b7fe1e3d
Ian Connolly <ian@connolly.io>
2014-11-28 21:53:56 +0000

    Cleanup, join properly

--- lib/threadpool.rb
+++ lib/threadpool.rb
@@ -1,8 +1,13 @@
 require "threadpool/version"
 
 module Threadpool
+
+  class ShutdownError < StandardError
+  end
+
   class Threadpool
     def initialize(workers:)
+      @shutdown = false
       @work_queue = Queue.new
       @worker_threads = (0...workers).map do
         Thread.new do
@@ -15,18 +20,29 @@ module Threadpool
     end
 
     def backlog
+      raise ShutdownError, "Threadpool already shutdown" if shutdown?
       @work_queue.size
     end
 
     def size
+      raise ShutdownError, "Threadpool already shutdown" if shutdown?
       @worker_threads.length
     end
 
-    def shutdown
-      @worker_threads.map(&:kill)
+    def shutdown?
+      @shutdown
+    end
+
+    def shutdown!(timeout: 0.2)
+      raise ShutdownError, "Threadpool already shutdown" if shutdown?
+      @worker_threads.map { |t|  
+        t.join timeout
+      }
+      @shutdown = true
     end
 
     def add_task(f, *args)
+      raise ShutdownError, "Threadpool already shutdown" if shutdown?
       @work_queue << [f, args]
     end
   end
--- test/thread_test.rb
+++ test/thread_test.rb
@@ -4,21 +4,25 @@ require "threadpool"
 
 
 class ThreadTest < Minitest::Unit::TestCase
+
+
+  def setup
+    @pool = Threadpool::Threadpool.new(workers: 4)
+  end
+
   def test_size
-    pool = Threadpool::Threadpool.new(workers: 4)
-    assert_equal 4, pool.size
+    assert_equal 4, @pool.size
   end
 
   def test_threads_single_arg
     func = Proc.new do |list, value|
       list << value
     end
-    pool = Threadpool::Threadpool.new(workers: 4)
     a = []
-    pool.add_task(func, a, 4)
+    @pool.add_task(func, a, 4)
     
     loop {
-      if pool.backlog == 0
+      if @pool.backlog == 0
         assert_equal [4], a
         break
       end
@@ -29,15 +33,22 @@ class ThreadTest < Minitest::Unit::TestCase
     func = Proc.new do |list, *values|
       list.push(*values)
     end
-    pool = Threadpool::Threadpool.new(workers: 4)
     a = []
-    pool.add_task(func, a, 4, 5, 6, 7, 8)
+    @pool.add_task(func, a, 4, 5, 6, 7, 8)
     
     loop {
-      if pool.backlog == 0
+      if @pool.backlog == 0
         assert_equal [4, 5, 6, 7, 8], a
         break
       end
     }
+  end
+
+  def test_shutdown
+    @pool.shutdown!
+    assert_equal @pool.shutdown?, true
+    assert_raises Threadpool::ShutdownError do
+      @pool.backlog
+    end
   end
 end
